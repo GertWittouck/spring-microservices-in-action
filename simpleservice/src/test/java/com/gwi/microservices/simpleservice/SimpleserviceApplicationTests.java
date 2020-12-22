bcaeafdf77371ca8e28ceda9ac531a038248a38e
Gert Wittouck <gertwittouck@gmail.com>
2020-12-22 07:30:24 +0000

    simple service - code cleanup

--- simpleservice/src/test/java/com/gwi/microservices/simpleservice/SimpleserviceApplicationTests.java
+++ simpleservice/src/test/java/com/gwi/microservices/simpleservice/SimpleserviceApplicationTests.java
@@ -1,25 +1,17 @@
 package com.gwi.microservices.simpleservice;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SimpleserviceApplicationTests {
-
-	private SimpleserviceApplication testable;
 
 	private MockMvc mockMvc;
 
