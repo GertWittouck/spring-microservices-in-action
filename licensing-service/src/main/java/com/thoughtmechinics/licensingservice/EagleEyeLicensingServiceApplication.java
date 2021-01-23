7b8fc6247a147a3e9a712867190abde619d4ae94
GertWittouck <gertwittouck@gmail.com>
2021-01-23 13:41:54 +0000

    GWI: add skeleton for licensing-service

---
+++ licensing-service/src/main/java/com/thoughtmechinics/licensingservice/EagleEyeLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechinics.licensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyeLicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleEyeLicensingServiceApplication.class, args);
+	}
+
+}
---
+++ licensing-service/src/test/java/com/thoughtmechinics/licensingservice/EagleEyeLicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtmechinics.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleEyeLicensingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
