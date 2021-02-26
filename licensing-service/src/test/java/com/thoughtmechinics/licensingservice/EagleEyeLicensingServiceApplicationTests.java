a4bad560f0e7efac80838b9731ef59cc2d36680c
Gert.Wittouck <Zeedijkstraat28>
2021-02-26 12:56:49 +0000

    GWI: add body to test to test Codacy integration

--- licensing-service/src/test/java/com/thoughtmechinics/licensingservice/EagleEyeLicensingServiceApplicationTests.java
+++ licensing-service/src/test/java/com/thoughtmechinics/licensingservice/EagleEyeLicensingServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.thoughtmechinics.licensingservice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class EagleEyeLicensingServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String loadingMessage = "Test context loads";
+		assertThat(loadingMessage).isEqualTo("Test context loads");
 	}
 
 }
