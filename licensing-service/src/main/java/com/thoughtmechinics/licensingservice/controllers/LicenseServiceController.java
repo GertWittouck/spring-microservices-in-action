37db70b7ef7d1dc981be7e437a127644127b4182
Gert.Wittouck <Zeedijkstraat28>
2021-03-23 17:46:51 +0000

    GWI: license-service - use services in controller

--- licensing-service/src/main/java/com/thoughtmechinics/licensingservice/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechinics/licensingservice/controllers/LicenseServiceController.java
@@ -1,25 +1,48 @@
 package com.thoughtmechinics.licensingservice.controllers;
 
 import com.thoughtmechinics.licensingservice.model.License;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtmechinics.licensingservice.services.LicenseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
 
-    @RequestMapping(value = "/{licenseId}", method = GET)
-    public License getLicenses(
-            @PathVariable("organizationId") String organizationId,
-            @PathVariable("licenseId") String licenseId
-    ) {
-        return new License()
-                .withId(licenseId)
-                .withProductName("Teleco")
-                .withLicenseType("Seat")
-                .withOrganizationId(organizationId);
+    private final LicenseService licenseService;
+
+    @RequestMapping(value="/",method = GET)
+    public List<License> getLicenses(@PathVariable("organizationId") String organizationId) {
+        return licenseService.getLicensesByOrg(organizationId);
+    }
+
+    @RequestMapping(value="/{licenseId}",method = GET)
+    public License getLicenses( @PathVariable("organizationId") String organizationId,
+                                @PathVariable("licenseId") String licenseId) {
+        return licenseService.getLicense(organizationId,licenseId);
+    }
+
+    @RequestMapping(value="{licenseId}",method = PUT)
+    public String updateLicenses(@PathVariable String organizationId,
+                                 @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the put with license ID %s and organization ID %s", licenseId, organizationId);
+    }
+
+    @RequestMapping(value="/",method = POST)
+    public void saveLicenses(@RequestBody License license) {
+        licenseService.saveLicense(license);
+    }
+
+    @RequestMapping(value="{licenseId}",method = DELETE)
+    @ResponseStatus(NO_CONTENT)
+    public String deleteLicenses(@PathVariable String organizationId,
+                                 @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the Delete with license ID %s and organization ID %s", licenseId, organizationId);
     }
 }
